fd71c82a4f897e7e05051dcdd253ddce4250cae4
Yancen Li <lee0741@gmail.com>
2017-02-28 15:32:55 +0900

    change tab to 2 spaces

--- JLPT N1 Grammar/ExampleCell.swift
+++ JLPT N1 Grammar/ExampleCell.swift
@@ -9,39 +9,39 @@
 import UIKit
 
 class ExampleCell: UITableViewCell {
+  
+  let jpLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.numberOfLines = 0
+    return label
+  }()
+  
+  let enLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.numberOfLines = 0
+    label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+    label.textColor = UIColor.gray
+    return label
+  }()
+  
+  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
     
-    let jpLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    let enLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 0
-        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        label.textColor = UIColor.gray
-        return label
-    }()
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        addSubview(jpLabel)
-        addSubview(enLabel)
-        jpLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16).isActive = true
-        jpLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
-        jpLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16).isActive = true
-        jpLabel.bottomAnchor.constraint(equalTo: enLabel.topAnchor, constant: -3).isActive = true
-        enLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16).isActive = true
-        enLabel.topAnchor.constraint(equalTo: jpLabel.bottomAnchor, constant: 3).isActive = true
-        enLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16).isActive = true
-        enLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    addSubview(jpLabel)
+    addSubview(enLabel)
+    jpLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16).isActive = true
+    jpLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+    jpLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16).isActive = true
+    jpLabel.bottomAnchor.constraint(equalTo: enLabel.topAnchor, constant: -3).isActive = true
+    enLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16).isActive = true
+    enLabel.topAnchor.constraint(equalTo: jpLabel.bottomAnchor, constant: 3).isActive = true
+    enLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16).isActive = true
+    enLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
--- JLPT N1 Grammar/HomeController.swift
+++ JLPT N1 Grammar/HomeController.swift
@@ -10,165 +10,166 @@ import UIKit
 import CoreData
 
 class HomeController: UITableViewController, UISearchResultsUpdating {
+  
+  let cellId = "homeCell"
+  var grammars = [GrammarMO]()
+  var grammarsDict = [String: [GrammarMO]]()
+  var grammarSectionTitles = [String]()
+  var searchController = UISearchController(searchResultsController: nil)
+  var searchResults = [GrammarMO]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configController()
     
-    let cellId = "homeCell"
-    var grammars = [GrammarMO]()
-    var grammarsDict = [String: [GrammarMO]]()
-    var grammarSectionTitles = [String]()
-    var searchController = UISearchController(searchResultsController: nil)
-    var searchResults = [GrammarMO]()
+  }
+  
+  func configController() {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configController()
+    navigationItem.title = "Grammar"
+    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    let bookmarkButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(HomeController.bookmarkAction))
+    navigationItem.setRightBarButton(bookmarkButtonItem, animated: true)
+    
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    tableView.sectionIndexColor = UIColor.black
+    tableView.sectionIndexBackgroundColor = UIColor.clear
+    
+    searchController.searchResultsUpdater = self
+    searchController.dimsBackgroundDuringPresentation = false
+    definesPresentationContext = true
+    searchController.searchBar.tintColor = UIColor.black
+    tableView.tableHeaderView = searchController.searchBar
+    tableView.contentOffset = CGPoint(x: 0, y: searchController.searchBar.frame.height)
+    searchController.loadViewIfNeeded()
+    
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Grammar")
+    grammars = (try! managedObjectContext.fetch(fetchRequest)) as! [GrammarMO]
+    
+    for grammar in grammars {
+      guard let grammarKey = grammar.acronym else {
+        return
+      }
+      
+      if grammarsDict[grammarKey] != nil {
+        grammarsDict[grammarKey]?.append(grammar)
+      } else {
+        grammarsDict[grammarKey] = [grammar]
+      }
     }
     
-    func configController() {
-        
-        navigationItem.title = "Grammar"
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        let bookmarkButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(HomeController.bookmarkAction))
-        navigationItem.setRightBarButton(bookmarkButtonItem, animated: true)
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        tableView.sectionIndexColor = UIColor.black
-        tableView.sectionIndexBackgroundColor = UIColor.clear
-        
-        searchController.searchResultsUpdater = self
-        searchController.dimsBackgroundDuringPresentation = false
-        definesPresentationContext = true
-        searchController.searchBar.tintColor = UIColor.black
-        tableView.tableHeaderView = searchController.searchBar
-        tableView.contentOffset = CGPoint(x: 0, y: searchController.searchBar.frame.height)
-        searchController.loadViewIfNeeded()
-        
-        let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Grammar")
-        grammars = (try! managedObjectContext.fetch(fetchRequest)) as! [GrammarMO]
-        
-        for grammar in grammars {
-            guard let grammarKey = grammar.acronym else {
-                return
-            }
-            
-            if grammarsDict[grammarKey] != nil {
-                grammarsDict[grammarKey]?.append(grammar)
-            } else {
-                grammarsDict[grammarKey] = [grammar]
-            }
-        }
-        
-        grammarSectionTitles = [String](grammarsDict.keys)
-        grammarSectionTitles.sort(by: { $0 < $1 })
-        
-        if traitCollection.forceTouchCapability == .available {
-            registerForPreviewing(with: self as UIViewControllerPreviewingDelegate, sourceView: view)
-        }
-        
+    grammarSectionTitles = [String](grammarsDict.keys)
+    grammarSectionTitles.sort(by: { $0 < $1 })
+    
+    if traitCollection.forceTouchCapability == .available {
+      registerForPreviewing(with: self as UIViewControllerPreviewingDelegate, sourceView: view)
     }
     
-    func bookmarkAction() {
-        present(UINavigationController(rootViewController: BookmarkController()), animated: true, completion: nil)
+  }
+  
+  func bookmarkAction() {
+    present(UINavigationController(rootViewController: BookmarkController()), animated: true, completion: nil)
+  }
+  
+  // MARK: - Table View Data Source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return searchController.isActive ? 1 : grammarSectionTitles.count
+  }
+  
+  override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    return searchController.isActive ? nil: grammarSectionTitles
+  }
+  
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return searchController.isActive ? nil : grammarSectionTitles[section]
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    if searchController.isActive {
+      return searchResults.count
+    } else {
+      let grammarKey = grammarSectionTitles[section]
+      
+      guard let grammarValues = grammarsDict[grammarKey] else {
+        return 0
+      }
+      
+      return grammarValues.count
     }
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 60
+  }
+  
+  override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let grammarKey = grammarSectionTitles[indexPath.section]
+    let grammarValues = grammarsDict[grammarKey]!
+    let grammar = searchController.isActive ? searchResults[indexPath.row] : grammarValues[indexPath.row]
     
-    // MARK: - Table View Data Source
+    cell.textLabel?.text = grammar.grammar
+    cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    cell.detailTextLabel?.text = grammar.meaning
+    cell.detailTextLabel?.textColor = UIColor.gray
+  }
+  
+  // MARK: - Table View Delegate
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let grammarKey = grammarSectionTitles[indexPath.section]
+    let grammarValues = grammarsDict[grammarKey]!
+    let grammar = searchController.isActive ? searchResults[indexPath.row] : grammarValues[indexPath.row]
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return searchController.isActive ? 1 : grammarSectionTitles.count
+    let detailController = DetailController()
+    detailController.grammar = grammar
+    navigationController?.pushViewController(detailController, animated: true)
+  }
+  
+  // MARK: - Search Logic
+  
+  func filterContent(for searchText: String) {
+    searchResults = grammars.filter { (grammar) -> Bool in
+      let isMatch = grammar.grammar!.localizedCaseInsensitiveContains(searchText) || grammar.meaning!.localizedCaseInsensitiveContains(searchText)
+      return isMatch
     }
-    
-    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return searchController.isActive ? nil: grammarSectionTitles
+  }
+  
+  func updateSearchResults(for searchController: UISearchController) {
+    if let searchText = searchController.searchBar.text {
+      filterContent(for: searchText)
+      tableView.reloadData()
     }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return searchController.isActive ? nil : grammarSectionTitles[section]
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        if searchController.isActive {
-            return searchResults.count
-        } else {
-            let grammarKey = grammarSectionTitles[section]
-            
-            guard let grammarValues = grammarsDict[grammarKey] else {
-                return 0
-            }
-            
-            return grammarValues.count
-        }
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-    
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let grammarKey = grammarSectionTitles[indexPath.section]
-        let grammarValues = grammarsDict[grammarKey]!
-        let grammar = searchController.isActive ? searchResults[indexPath.row] : grammarValues[indexPath.row]
-        
-        cell.textLabel?.text = grammar.grammar
-        cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
-        cell.detailTextLabel?.text = grammar.meaning
-        cell.detailTextLabel?.textColor = UIColor.gray
-    }
-    
-    // MARK: - Table View Delegate
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let grammarKey = grammarSectionTitles[indexPath.section]
-        let grammarValues = grammarsDict[grammarKey]!
-        let grammar = searchController.isActive ? searchResults[indexPath.row] : grammarValues[indexPath.row]
-        
-        let detailController = DetailController()
-        detailController.grammar = grammar
-        navigationController?.pushViewController(detailController, animated: true)
-    }
-    
-    // MARK: - Search Logic
-    
-    func filterContent(for searchText: String) {
-        searchResults = grammars.filter { (grammar) -> Bool in
-            let isMatch = grammar.grammar!.localizedCaseInsensitiveContains(searchText) || grammar.meaning!.localizedCaseInsensitiveContains(searchText)
-            return isMatch
-        }
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        if let searchText = searchController.searchBar.text {
-            filterContent(for: searchText)
-            tableView.reloadData()
-        }
-    }
-    
+  }
+  
 }
 
 // MARK: - 3D Touch
 
 extension HomeController: UIViewControllerPreviewingDelegate {
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
-        
-        guard let indexPath = tableView.indexPathForRow(at: location),
-            let cell = tableView.cellForRow(at: indexPath),
-            let grammar = grammarsDict[grammarSectionTitles[indexPath.section]]?[indexPath.row] else {
-                return nil
-        }
-        
-        let detailController = DetailController()
-        detailController.grammar = grammar
-        previewingContext.sourceRect = cell.frame
-        
-        return detailController
+  func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+    
+    guard let indexPath = tableView.indexPathForRow(at: location),
+      let cell = tableView.cellForRow(at: indexPath),
+      let grammar = grammarsDict[grammarSectionTitles[indexPath.section]]?[indexPath.row] else {
+        return nil
     }
     
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
-        navigationController?.pushViewController(viewControllerToCommit, animated: true)
-    }
+    let detailController = DetailController()
+    detailController.grammar = grammar
+    previewingContext.sourceRect = cell.frame
+    
+    return detailController
+  }
+  
+  func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+    navigationController?.pushViewController(viewControllerToCommit, animated: true)
+  }
 }
